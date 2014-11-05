da78d38702933820cbb27d0dfbfbb2193749cb48
deng <linlongdeng@163.com>
2014-11-05 17:07:21 +0800

    1、增加Oracle属性对话框 2、还未做Oracle的属性文件读写

--- ExecuteSqlTools/src/org/deng/OracleDialog.java
+++ ExecuteSqlTools/src/org/deng/OracleDialog.java
@@ -9,6 +9,10 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 
 public class OracleDialog extends Dialog {
 
@@ -17,6 +21,14 @@ public class OracleDialog extends Dialog {
 	private Text text;
 	private Label label;
 	private Text text_1;
+	private Label lblNewLabel_1;
+	private Text text_2;
+	private Label label_1;
+	private Text text_3;
+	private Label label_2;
+	private Text text_4;
+	private Button button;
+	private Button button_1;
 
 	/**
 	 * Create the dialog.
@@ -70,7 +82,7 @@ public class OracleDialog extends Dialog {
 		shell.setText("\u6570\u636E\u5E93\u914D\u7F6E");
 
 		GridLayout gl_shell = new GridLayout();
-		gl_shell.numColumns = 2;
+		gl_shell.numColumns = 4;
 		gl_shell.marginTop = 10;
 		gl_shell.marginRight = 30;
 		gl_shell.marginLeft = 10;
@@ -87,16 +99,57 @@ public class OracleDialog extends Dialog {
 		lblNewLabel.setText("IP:");
 
 		text = new Text(shell, SWT.BORDER);
-		text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-
-		label = new Label(shell, SWT.NONE);
-		label.setText("\u7AEF\u53E3\uFF1A");
-
-		text_1 = new Text(shell, SWT.BORDER);
-		GridData gd_text_1 = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1,
-				1);
-		gd_text_1.widthHint = 100;
-		text_1.setLayoutData(gd_text_1);
+		text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 3, 1));
+				
+						label = new Label(shell, SWT.NONE);
+						label.setText("\u7AEF\u53E3:");
+				
+						text_1 = new Text(shell, SWT.BORDER);
+						text_1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1,
+								1));
+		
+		lblNewLabel_1 = new Label(shell, SWT.NONE);
+		lblNewLabel_1.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblNewLabel_1.setText("\u670D\u52A1\u540D:");
+		
+		text_2 = new Text(shell, SWT.BORDER);
+		text_2.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		label_1 = new Label(shell, SWT.NONE);
+		label_1.setText("\u7528\u6237\u540D:");
+		
+		text_3 = new Text(shell, SWT.BORDER);
+		text_3.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		label_2 = new Label(shell, SWT.NONE);
+		label_2.setText("\u5BC6\u7801:");
+		
+		text_4 = new Text(shell, SWT.BORDER | SWT.PASSWORD);
+		text_4.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		button = new Button(shell, SWT.NONE);
+		button.setFont(SWTResourceManager.getFont("Î¢ÈíÑÅºÚ", 11, SWT.NORMAL));
+		GridData gd_button = new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1);
+		gd_button.horizontalIndent = 10;
+		button.setLayoutData(gd_button);
+		button.setText("\u786E\u5B9A");
+		
+		button_1 = new Button(shell, SWT.NONE);
+		button_1.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				shell.close();
+			}
+		});
+		GridData gd_button_1 = new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1);
+		gd_button_1.horizontalIndent = 5;
+		button_1.setLayoutData(gd_button_1);
+		button_1.setFont(SWTResourceManager.getFont("Î¢ÈíÑÅºÚ", 11, SWT.NORMAL));
+		button_1.setText("\u53D6\u6D88");
+		new Label(shell, SWT.NONE);
+		new Label(shell, SWT.NONE);
+		new Label(shell, SWT.NONE);
+		new Label(shell, SWT.NONE);
 
 	}
 
